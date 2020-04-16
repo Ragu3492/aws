bd3d1c7a51e65707b88221096c3e5bb74b1245f7
Ragu3492 <34977851+Ragu3492@users.noreply.github.com>
2020-04-16 16:35:48 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,32 @@
+ #!/bin/bash
+ sudo amazon-linux-extras install epel -y
+ sudo yum install epel-release -y
+ sudo yum install git -y
+ sudo yum update -y
+ .
+ .
+ 
+ .
+ .
+ .
+ .
+ 
+ .
+ .
+ .
+ 
+ .
+ .
+ .
+ .
+ .
+ 
+ .
+ .
+ .
+ .
+ .
+ ..
+ 
+ .
+ 
