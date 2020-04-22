8f7d02548aa37de66bf947fd67d6f1955f0761d2
Ragu3492 <34977851+Ragu3492@users.noreply.github.com>
2020-04-22 14:13:22 +0530

    Update ragu.sh

--- ragu.sh
+++ ragu.sh
@@ -42,13 +42,12 @@ sudo wp core install --url=$ip --title=zippyops --admin_user=zippyops --admin_pa
 sudo chown -R apache /var/www/html
 wp theme install Consulting --allow-root
 wp theme activate consulting --allow-root
-
+wp plugin install wordpress-importer --activate --allow-root
 
 cd 
 sudo git clone https://github.com/Ragu3492/xml.git
 sudo cp /root/xml/zippyopssite.wordpress.2020-04-22.000.xml /var/www/html/
 cd /var/www/html
-wp plugin install wordpress-importer --activate --allow-root
 wp import zippyopssite.wordpress.2020-04-22.000.xml --authors=create --allow-root
 
 sudo systemctl restart httpd
