fe44dd3ca94e4b819fd073397cbcdb37722f7969
Ragu3492 <34977851+Ragu3492@users.noreply.github.com>
2020-04-22 13:39:59 +0530

    Update ragu.sh

--- ragu.sh
+++ ragu.sh
@@ -46,8 +46,9 @@ wp theme activate consulting --allow-root
 
 cd 
 sudo git clone https://github.com/Ragu3492/xml.git
-sudo cp /xml/zippyopssite.wordpress.2020-04-22.000.xml /var/www/html
+sudo cp /root/xml/zippyopssite.wordpress.2020-04-22.000.xml /var/www/html/
 cd /var/www/html
-sudo wp plugin install wordpress-importer --activate --allow-root
+wp plugin install wordpress-importer --activate --allow-root
 wp import zippyopssite.wordpress.2020-04-22.000.xml --authors=create --allow-root
+
 sudo systemctl restart httpd
