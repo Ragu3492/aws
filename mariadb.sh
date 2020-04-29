eedc7ffa85c2877ff3804c3e79d224cd4e754c42
Ragu3492 <34977851+Ragu3492@users.noreply.github.com>
2020-04-29 19:49:58 +0530

    Add files via upload

---
+++ mariadb.sh
@@ -0,0 +1,12 @@
+sudo yum update -y
+sudo yum install epel-release -y
+sudo yum -y install mariadb-server
+sudo systemctl start mariadb && sudo systemctl enable mariadb
+
+#MySQL Consider changing values from drupaldb, drupaluser, and password
+sudo echo "CREATE DATABASE zippyopsdb CHARACTER SET utf8 COLLATE utf8_general_ci;;" | mysql
+sudo echo "CREATE USER 'zippyops'@'localhost' IDENTIFIED BY 'zippyops';" | mysql
+sudo echo "GRANT ALL PRIVILEGES ON zippyopsdb.* TO 'zippyops'@'localhost';" | mysql
+sudo echo "FLUSH PRIVILEGES;" | mysql
+
+systemctl restart mariadb
